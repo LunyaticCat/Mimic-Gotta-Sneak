07458127668e98182a3ab8ae7efa2b3f4edf6418
thomas-subsol <thomas.subsol@etu.umontpellier.fr>
2021-01-19 15:23:26 +0100

    frgt

--- Assets/Scripts/Ennemis/EnnDetection.cs
+++ Assets/Scripts/Ennemis/EnnDetection.cs
@@ -1,34 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class EnnDetection : MonoBehaviour
 {
-    bool CanSeePlayer()
+    public float viewDistance = 5;
+    public float viewAngle = 30;
+    private GameObject _mimic;
+    private EnnMovement _ennMovement;
+    private float _playerEnemyDistance;
+    private bool _playerAtRight = true;
+    private bool _playerInView = false;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        RaycastHit hit;
-        var rayDirection = playerObject.transform.position - transform.position;
-        if (Physics.Raycast(transform.position, rayDirection, hit))
+        _mimic = GameObject.FindGameObjectWithTag("Player");
+        _ennMovement = GetComponent<EnnMovement>();
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x - _mimic.transform.position.x < 0)
         {
-            if (hit.transform.tag == "Player")
-            {
-                Debug.Log("En vue");
-                return true;
-            }
-            else
-            {
-                Debug.Log("Pas en vue");
-                return false;
-            }
+            _playerEnemyDistance = Math.Abs(transform.position.x - _mimic.transform.position.x);
+            _playerAtRight = false;
+        }
+        else
+        {
+            _playerEnemyDistance = transform.position.x - _mimic.transform.position.x;
+            _playerAtRight = true;
         }
 
-        // Start is called before the first frame update
-        void Start()
+        if (inViewField())
         {
+            (Math.PI / 180) * 
+            if (Math.Tan());
+            _playerInView = true;
         }
-
-        // Update is called once per frame
-        void Update()
+        else
         {
+            _playerInView = false;
         }
     }
+
+    bool inViewField()
+    {
+        if (_playerEnemyDistance < viewDistance)
+        {
+            return _ennMovement.GETFaceLeft() && _playerAtRight || !_ennMovement.GETFaceLeft() && !_playerAtRight;
+        }
+
+        return false;
+    }
+
+    bool getPlayerInView()
+    {
+        return _playerInView;
+    }
+}
