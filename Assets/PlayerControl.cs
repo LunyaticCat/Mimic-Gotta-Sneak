cbd0d600123a98c2ee4e1aee7cc0485645481788
LunaticCat <yannbrun1507@outlook.fr>
2021-01-19 11:47:58 +0100

    Starting movement

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rb;
+    public float speed;
+    public float jump;
+    private float move;
 
-    // Update is called once per frame
+    private bool canJump = true;
+
     void Update()
     {
-        
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+         void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.tag == "Ground")
+            {
+                canJump = false;
+                Debug.Log("can't jump");
+            }
+        }
+
+         void OnCollisionExit(Collision collision)
+        {
+            if (collision.collider.tag == "Ground")
+            {
+                canJump = true;
+                Debug.Log("can jump");
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
     }
 }
